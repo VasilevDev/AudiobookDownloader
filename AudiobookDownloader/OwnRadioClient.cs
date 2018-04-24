b9383fb8f3c0217396d9be5859d2096a57653c9e
Alexey Vasilev <valexey.dev@gmail.com>
2018-04-24 17:15:11 +0600

    добавил класс для загрузки аудиофайлов в ownradio

---
+++ AudiobookDownloader/OwnRadioClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AudiobookDownloader
+{
+	class OwnRadioClient
+	{
+		private readonly HttpClient _client;
+
+		public OwnRadioClient()
+		{
+			_client = new HttpClient();
+		}
+
+		public async Task<bool> Upload(AudioBook audiobook)
+		{
+			foreach (var file in audiobook.Files)
+			{
+				Debug.Print($"Отправили файл {file.Name} с id {file.Id} размером {file.Content.Length} байт");
+			}
+			
+			return false;
+		}
+	}
+}
