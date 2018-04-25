2416a1da78c53c3f0338719716f43ddb9600fbcb
Alexey Vasilev <valexey.dev@gmail.com>
2018-04-25 14:54:12 +0600

    добавил БД SQLite для хранения истории отдачи треков

---
+++ AudiobookDownloader/DatabaseContext/SQLiteContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using SQLite.CodeFirst;
+
+namespace AudiobookDownloader.DatabaseContext
+{
+	class SQLiteContext : DbContext
+	{
+		public SQLiteContext()
+			: base(@"AudioFileDb")
+		{
+
+		}
+
+		public DbSet<AudioBook> Audiobooks { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<SQLiteContext>(modelBuilder);
+			Database.SetInitializer(sqliteConnectionInitializer);
+		}
+	}
+}
