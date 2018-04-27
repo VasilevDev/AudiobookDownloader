64a9fea8c865b9f61ec1f2e510e0adc2bc4cc037
Alexey Vasilev <valexey.dev@gmail.com>
2018-04-27 15:43:04 +0600

    изменил интерфейс взаимодействия с аудиокнигой

--- AudiobookDownloader/Audiobook.cs
+++ AudiobookDownloader/Audiobook.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace AudiobookDownloader
 {
-	class AudioBook
+	class Audiobook
 	{
-		public int Id { get; set; }
-		public string Url { get; set; }
 		public string Title { get; set; }
-
-		public List<AudioFile> Files { get; set; }
+		public string Url { get; set; }
 	}
 }
