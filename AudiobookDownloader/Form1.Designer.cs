33a535ddef6fffcf8bbfe22df1f3977300fc85a5
Alexey Vasilev <valexey.dev@gmail.com>
2018-04-27 15:45:13 +0600

    вынес вызов методов сервиса и грабера

--- AudiobookDownloader/Form1.Designer.cs
+++ AudiobookDownloader/Form1.Designer.cs
@@ -31,6 +31,7 @@
 			this.ListOfCategories = new System.Windows.Forms.ListBox();
 			this.AbooksBtn = new System.Windows.Forms.Button();
 			this.label = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// ListOfCategories
@@ -62,11 +63,22 @@
 			this.label.Size = new System.Drawing.Size(0, 17);
 			this.label.TabIndex = 2;
 			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(735, 463);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(134, 33);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(881, 511);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label);
 			this.Controls.Add(this.AbooksBtn);
 			this.Controls.Add(this.ListOfCategories);
@@ -83,6 +95,7 @@
 		private System.Windows.Forms.ListBox ListOfCategories;
 		private System.Windows.Forms.Button AbooksBtn;
 		private System.Windows.Forms.Label label;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
--- AudiobookDownloader/Form1.cs
+++ AudiobookDownloader/Form1.cs
@@ -1,66 +1,69 @@
 ﻿using AudiobookDownloader.Service;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Compression;
 using System.Windows.Forms;
 
 namespace AudiobookDownloader
 {
 	public partial class Form1 : Form
 	{
-		private readonly AbooksService _abookService;
+		private readonly IAudiobookService _service = new AbooksService();
+		private readonly Grabber _grabber;
+		private List<Category> _categories = new List<Category>();
+		private List<Audiobook> _audiobooks = new List<Audiobook>();
 
 		public Form1()
 		{
 			InitializeComponent();
-			_abookService = new AbooksService();
+			_grabber = new Grabber(_service);
 		}
 
 		private async void AbooksBtn_Click(object sender, EventArgs e)
 		{
-			var _categories = await _abookService.GetCategories();
-
-			if(_categories == null)
-			{
-				label.Text = "Ошибка подключения к Abooks порталу";
-				return;
-			}
+			_categories = await _service.GetCategories();
 
 			label.Text = "Категории";
 			ListOfCategories.Items.Clear();
 
 			foreach (var item in _categories)
-			{
-				ListOfCategories.Items.Add(item);
-			}
+				ListOfCategories.Items.Add(item.Name);
 		}
 
 		private async void ListOfCategories_DoubleClick(object sender, EventArgs e)
 		{
-			bool res = await _abookService.DownloadAudioBooks(ListOfCategories.SelectedItem.ToString());
-
-			/*var _books = await _abookService.GetAudiobooks(ListOfCategories.SelectedItem.ToString());
-			string _books = null;
-
-			if (_books == null)
-			{
-				label.Text = "Ошибка получения книг";
-				return;
-			}
-
-			label.Text = $"Книги: {_books}";
-
+			var category = _categories.Find(c => c.Name == ListOfCategories.SelectedItem.ToString());
+			_audiobooks = await _service.GetAudiobooks(category, 1);
 			ListOfCategories.Items.Clear();
 
-			foreach (var item in _books)
+			foreach (var audiobook in _audiobooks)
 			{
-				ListOfCategories.Items.Add(item);
-			}*/
+				await _grabber.Grab(audiobook);
+			}
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			OwnRadioClient client = new OwnRadioClient();
+
+			Guid ownerRecId = Guid.NewGuid();
+
+			using (var zip = ZipFile.OpenRead("tmp.zip"))
+			{
+				int chapter = 0;
+
+				foreach (var entry in zip.Entries)
+				{
+					if (entry.FullName.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase))
+					{
+						using (var fs = entry.Open())
+						{
+							Guid recId = Guid.NewGuid();
+							await client.Upload(fs, ++chapter, entry.Name, ownerRecId, recId);
+						}
+					}
+				}
+			}
 		}
 	}
 }
