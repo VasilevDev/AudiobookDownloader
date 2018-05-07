4f360e160abd82667d05aa134b418469e908b4cf
Alexey Vasilev <valexey.dev@gmail.com>
2018-05-07 18:10:05 +0600

    добавил сохранение аудиофайлов отправленных на рдев и соответствующие проверки

--- AudiobookDownloader/Grabber.cs
+++ AudiobookDownloader/Grabber.cs
@@ -2,6 +2,7 @@
 using AudiobookDownloader.Service;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -22,20 +23,23 @@ namespace AudiobookDownloader
 			_service = service;
 			_db = new Context();
 		}
-
+		
 		public async Task Grab(Audiobook audiobook)
 		{
-			await DownloadAudiobook(audiobook);
-			await UploadAudiobook(audiobook);
+			await Download(audiobook);
+			await Upload(audiobook);
 		}
 
-		private async Task DownloadAudiobook(Audiobook audiobook)
+		private async Task Download(Audiobook audiobook)
 		{
-			List<DownloadAudiobook> downloadList = null;
+			List<Audiobook> downloadList = null;
 
 			if (_db.DownloadAudiobook.Count() != 0)
 			{
-				downloadList = _db.DownloadAudiobook.Where(m => m.Audiobook.Title == audiobook.Title && m.Audiobook.Url == audiobook.Url).ToList();
+				downloadList = _db.DownloadAudiobook.Select(m => m.Audiobook).Where(m => 
+					m.Title == audiobook.Title && 
+					m.Url == audiobook.Url
+				).ToList();
 			}
 
 			if (downloadList == null || downloadList.Count == 0)
@@ -49,42 +53,69 @@ namespace AudiobookDownloader
 			}
 		}
 
-		private async Task UploadAudiobook(Audiobook audiobook)
+		private async Task Upload(Audiobook audiobook)
 		{
-			List<UploadAudiobook> uploadList = null;
-
 			if (_db.UploadAudiobook.Count() != 0)
 			{
-				uploadList = _db.UploadAudiobook.Where(m => m.Audiobook.Title == audiobook.Title && m.Audiobook.Url == audiobook.Url).ToList();
+				var dbAudiobooks = _db.UploadAudiobook.Select(m => m.Audiobook).Where(m => m.Title == audiobook.Title && m.Url == audiobook.Url).ToList();
+
+				if (dbAudiobooks.Count > 0)
+					return;
 			}
 
-			if (uploadList == null || uploadList.Count == 0)
+			Guid ownerRecId;
+
+			if (_db.UploadAudiofile.Count() > 0)
 			{
-				Guid ownerRecId = Guid.NewGuid();
+				var files = _db.UploadAudiofile.Select(m => m.File).Where(m => m.AudiobookUrl == audiobook.Url).ToList();
+				ownerRecId = (files.Count > 0) ? Guid.Parse(files[0].OwnerRecid) : Guid.NewGuid();
+			}
+			else
+			{
+				ownerRecId = Guid.NewGuid();
+			}
 
-				_db.UploadAudiobook.Add(new UploadAudiobook { Audiobook = audiobook });
-				await _db.SaveChangesAsync();
+			using (var zip = ZipFile.OpenRead(_filename))
+			{
+				int chapter = 0;
 
-				
-				using (var zip = ZipFile.OpenRead(_filename))
+				foreach (var entry in zip.Entries)
 				{
-					int chapter = 0;
-
-					foreach (var entry in zip.Entries)
+					if (entry.FullName.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase))
 					{
-						if (entry.FullName.EndsWith(".mp3", StringComparison.OrdinalIgnoreCase))
+						using (var fs = entry.Open())
 						{
-							using (var fs = entry.Open())
-							{
-								Guid recId = Guid.NewGuid();
-								await _client.Upload(fs, ++chapter, entry.Name, ownerRecId, recId);
+							Guid recId = Guid.NewGuid();
 
-								_db.UploadAudiobook.Add(new UploadAudiobook { Audiobook = audiobook });
-								_db.SaveChanges();
+							var file = new Audiofile()
+							{
+								Name = entry.Name,
+								Chapter = ++chapter,
+								OwnerRecid = ownerRecId.ToString(),
+								AudiobookUrl = audiobook.Url
+							};
+
+							var files = _db.UploadAudiofile.Select(m => m.File).Where(m => 
+								m.Name == entry.Name && 
+								m.Chapter == chapter &&
+								m.OwnerRecid == ownerRecId.ToString()
+							).ToList();
+
+							if (files.Count > 0)
+							{
+								continue;
 							}
+
+							await _client.Upload(file, fs, recId);
+
+							_db.UploadAudiofile.Add(new UploadAudiofile { File = file });
+							await _db.SaveChangesAsync();
 						}
 					}
 				}
+
+				_db.UploadAudiobook.Add(new UploadAudiobook { Audiobook = audiobook });
+				await _db.SaveChangesAsync();
 			}
 		}
 	}
