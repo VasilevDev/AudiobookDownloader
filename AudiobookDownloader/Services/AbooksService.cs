1db71a97ed74824abfa970f872eecdee1fd91f1b
Alexey Vasilev <valexey.dev@gmail.com>
2018-07-16 16:43:09 +0600

    добавлено использование прокси

--- AudiobookDownloader/Services/AbooksService.cs
+++ AudiobookDownloader/Services/AbooksService.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net.Http;
 using System.Threading.Tasks;
 using AngleSharp.Parser.Html;
 using System.Net;
@@ -16,85 +15,74 @@ namespace AudiobookDownloader.Service
 
 		public async Task<List<Category>> GetCategories()
 		{
-			using (var http = new HttpClient())
-			using (var response = await http.GetAsync(_baseUrl).ConfigureAwait(false))
+			string html = await GetHtml(_baseUrl);
+			var result = _parser.Parse(html);
+			var categories = result.GetElementsByClassName("mfn-megamenu-title");
+
+			var list = new List<Category>();
+
+			foreach (var item in categories)
 			{
-				var html = await response.Content.ReadAsStringAsync();
-				var result = _parser.Parse(html);
-				var categories = result.GetElementsByClassName("mfn-megamenu-title");
-
-				var list = new List<Category>();
-
-				foreach (var item in categories)
+				list.Add(new Category
 				{
-					list.Add(new Category
-					{
-						Name = item.TextContent,
-						Url = item.GetAttribute("href")
-					});
-				}
-
-				return list;
+					Name = item.TextContent,
+					Url = item.GetAttribute("href")
+				});
 			}
+
+			return list;
+			
 		}
 
 		public async Task<List<Audiobook>> GetAudiobooks(Category category, int page)
 		{
-			using (var http = new HttpClient())
-			using (var response = await http.GetAsync($"{category.Url}/page/{page}").ConfigureAwait(false))
+			string html = await GetHtml(category.Url);
+			var result = _parser.Parse(html);
+			var audiobooks = result.GetElementsByClassName("post-desc");
+
+			var list = new List<Audiobook>();
+
+			foreach (var audiobook in audiobooks)
 			{
-				var html = await response.Content.ReadAsStringAsync();
-				var result = _parser.Parse(html);
-				var audiobooks = result.GetElementsByClassName("post-desc");
-
-				var list = new List<Audiobook>();
-
-				foreach (var audiobook in audiobooks)
+				list.Add(new Audiobook
 				{
-					list.Add(new Audiobook
-					{
-						Title = audiobook.GetElementsByClassName("entry-title")[0].TextContent,
-						Url = audiobook.GetElementsByClassName("post-more")[0].GetAttribute("href")
-					});
-				}
-
-				return list;
+					Title = audiobook.GetElementsByClassName("entry-title")[0].TextContent,
+					Url = audiobook.GetElementsByClassName("post-more")[0].GetAttribute("href")
+				});
 			}
+
+			return list;
 		}
 
 		public async Task GetAudiobook(Audiobook audiobook, Stream stream)
 		{
 			int id = await GetAudiobookId(audiobook);
 
-			HttpWebRequest httpRequest = (HttpWebRequest) WebRequest.Create($"https://aobooks.zone/download/{id}");
-			httpRequest.Method = WebRequestMethods.Http.Get;
-			HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse();
-			Stream httpResponseStream = httpResponse.GetResponseStream();
+			HttpWebRequest request = (HttpWebRequest) WebRequest.Create($"https://aobooks.zone/download/{id}");
+			request.Proxy = new WebProxy("101.108.255.99", 8080);
 
-			await httpResponseStream.CopyToAsync(stream);
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+			Stream responseStream = response.GetResponseStream();
+
+			await responseStream.CopyToAsync(stream);
 		}
 
 		public async Task<int> GetPagesCount(Category category)
 		{
+			string html = await GetHtml(category.Url);
+			var result = _parser.Parse(html);
+
+			var pages = result.GetElementsByClassName("page");
+
 			int lastPage = 0;
 
-			using (var http = new HttpClient())
+			foreach (var page in pages)
 			{
-				using (var response = await http.GetAsync(category.Url).ConfigureAwait(false))
+				if (Int32.TryParse(page.TextContent, out int pageNumber))
 				{
-					var html = await response.Content.ReadAsStringAsync();
-					var result = _parser.Parse(html);
-					var pages = result.GetElementsByClassName("page");
-
-					foreach (var page in pages)
+					if (pageNumber > lastPage)
 					{
-						if (Int32.TryParse(page.TextContent, out int pageNumber))
-						{
-							if (pageNumber > lastPage)
-							{
-								lastPage = pageNumber;
-							}
-						}
+						lastPage = pageNumber;
 					}
 				}
 			}
@@ -104,16 +92,26 @@ namespace AudiobookDownloader.Service
 
 		public async Task<int> GetAudiobookId(Audiobook audiobook)
 		{
-			using (var http = new HttpClient())
-			using (var response = await http.GetAsync($"{audiobook.Url}").ConfigureAwait(false))
-			{
-				var html = await response.Content.ReadAsStringAsync();
-				var result = _parser.Parse(html);
-				string uri = result.GetElementsByClassName("button button_js button_green")[0].GetAttribute("href");
-				Int32.TryParse(HttpUtility.ParseQueryString(new Uri(uri).Query).Get("book_id"), out int id);
+			string html = await GetHtml(audiobook.Url);
+			var result = _parser.Parse(html);
+			string uri = result.GetElementsByClassName("button button_js button_green")[0].GetAttribute("href");
+			Int32.TryParse(HttpUtility.ParseQueryString(new Uri(uri).Query).Get("book_id"), out int id);
 
-				return id;
-			}
+			return id;
+		}
+
+		private async Task<string> GetHtml(string url)
+		{
+			HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+			request.Proxy = new WebProxy("101.108.255.99", 8080);
+
+			HttpWebResponse response = await request.GetResponseAsync() as HttpWebResponse;
+			var stream = response.GetResponseStream();
+
+			StreamReader reader = new StreamReader(stream);
+			string html = await reader.ReadToEndAsync();
+
+			return html;
 		}
 	}
 }
